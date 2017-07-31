c66589895a257ad5d7f3b489f62b1cf61d106302
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-07-30 21:34:33 -0700

    Switch to imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	minifyCSS = require('gulp-minify-css'),
-	tinify = require('gulp-tinify')
+	imagemin = require('gulp-imagemin'),
 	htmlmin = require('gulp-htmlmin'),
 	svgmin = require('gulp-svgmin'),
 	critical = require('critical'),
@@ -63,7 +63,9 @@ gulp.task('styles', function(){
 
 gulp.task('images', function(){
 	return gulp.src(paths.images)
-	.pipe(tinify('idMQZa1hEHf2OYTiDVgpmn3snhlNBzmo'))
+	.pipe(imagemin([
+		imagemin.jpegtran({progressive: true})
+	]))
 	.pipe(gulp.dest('dist/images/'))
 });
 
